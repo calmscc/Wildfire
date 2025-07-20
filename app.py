279bce46485c0c18268cc9fa2502841677180d76
Caleb Maxie <cam71805@gmail.com>
2025-07-20 10:38:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,9 +80,7 @@ st.markdown("<h2 style='text-align: center; font-size: 35px;'>Wildfire Risk Pred
 st.markdown("<h2 style='text-align: center; font-size: 17.5px;'>Enter the weather and environmental data to predict wildfire risk.</h2>", unsafe_allow_html=True)
 
 # --- Input Form ---
-with st.container():
-    st.markdown('<div class="input-container">', unsafe_allow_html=True)
-
+with st.form("fire_form"):
     col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 
     with col1:
